f4be765fe47a55ab6b79f9d9d689e97e5b9c5780
Bryan D. Gonzalez <bryan.dujari@gmail.com>
2020-08-21 18:42:00 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -16,19 +16,25 @@ def third_challenge
         :name => "Romeo",
         :age => "15",
         :status => "alive"
-
         },
       :hero_friends => []
    },
    :capulet => {
       :patriarch => {
         #your key/value pairs here
+        :name => "Lord Capulet",
+        :age => "50"
         },
       :matriarch => {
         #your key/value pairs here
+        :name => "Lord Capulet",
+        :age => "51"
         },
       :heroine => {
         #your key/value pairs here
+        :name => "Juliet",
+        :age => "15",
+        :status => "alive"
         },
       :heroine_friends => []
    }
